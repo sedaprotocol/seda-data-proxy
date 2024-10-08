ef6857418512547bf75460f10123db9e7e28f9ef
kaynetik <aleksandar@nesovic.dev>
2024-10-08 15:15:10 +0200

    fix: Improve release to handle primary architectures
    - Switch to matrix
    - Cover arm and amd builds

--- .build/docker/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -f "/app/config.json" ] && [ -f "/app/data-proxy-private-key.json" ]; then
-  echo "Config and private key files found. Running with specific configuration."
-  exec ./dataproxy run --config /app/config.json --private-key-file /app/data-proxy-private-key.json "$@"
-else
-  echo "Config or private key file not found. Running with default configuration."
-  exec ./dataproxy run "$@"
-fi
